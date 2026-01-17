3ba2656255ccd826058965e8ade86bdcd47fa934
Mohit52jaya <arman.tannumanisha52@gmail.com>
2026-01-18 03:50:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
-import App from "./App";
-import "@/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/aso-publication">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
