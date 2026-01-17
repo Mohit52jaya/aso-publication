9ba39ecd4ddee14c4603d4e76d21af85f19fa1ba
Mohit52jaya <arman.tannumanisha52@gmail.com>
2026-01-18 04:52:51 +0530

    Replace BrowserRouter with HelmetProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HelmetProvider } from "react-helmet-async";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/aso-publication">
+    <HelmetProvider>
       <App />
-    </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
