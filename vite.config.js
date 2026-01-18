c4da1d265c6034d81308996632d378632f000b39
Mohit52jaya <arman.tannumanisha52@gmail.com>
2026-01-18 16:35:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/aso-publication/",   // keep ONLY if repo name = aso-publication
+  base: "/",   // REQUIRED for custom domain
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
