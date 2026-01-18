0653cb347b1be6eb68d60f40dac63038ebd9d0aa
Mohit52jaya <arman.tannumanisha52@gmail.com>
2026-01-18 17:45:50 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -108,7 +108,7 @@ const HomePage = () => {
             Featured Products
           </h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 gap-3">
             {featuredProducts.map((product) => (
               <ProductCard
                 key={product.id}
